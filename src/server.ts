8e2539a87a25638e02a51756cfa47db7beaa2a26
Igor Fonseca <igorfonsecaigor@gmail.com>
2020-08-10 18:56:24 -0300

    Adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors);
 // Utilizar formato JSON no express
 app.use(express.json());
 app.use(routes);
