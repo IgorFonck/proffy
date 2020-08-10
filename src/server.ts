2292a72e1b2b502583de1826402409415038bb6d
Igor Fonseca <igorfonsecaigor@gmail.com>
2020-08-10 17:02:37 -0300

    Criando aulas no banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 // Utilizar formato JSON no express
 app.use(express.json());
-
-// Cria a rota /users na aplicação
-// request: parâmetros da requisição (cabeçalho, corpo...)
-app.get("/", (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 // Faz a aplicação ouvir requisições HTTP
 app.listen(3333);
