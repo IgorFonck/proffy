52ecdb882e579e30fcf7ff8d1dfb6fd0d2f0da29
Igor Fonseca <igorfonsecaigor@gmail.com>
2020-08-11 17:51:42 -0300

    Enviando mensagem no whatsapp e contando novas conexões

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,8 +44,8 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
               <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button"><img src={whatsappIcon} alt="WhatsApp" />
-            Entrar em contato</button>
+        <a onClick={createNewConnection} target="_blank" href={`https://wa.me/${teacher.whatsapp}`}><img src={whatsappIcon} alt="WhatsApp" />
+            Entrar em contato</a>
       </footer>
     </article>
   )
