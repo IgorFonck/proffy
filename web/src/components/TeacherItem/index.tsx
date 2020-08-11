84020cd9931c4c7a0cd670290587b4009ec70624
Igor Fonseca <igorfonsecaigor@gmail.com>
2020-08-11 17:39:39 -0300

    Mostrando lista de professores no front end de acordo com filtros do usuário'

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,27 +4,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/9698161?s=460&u=c2de3a852babda47a149ca785fb6ed7f54ca62f4&v=4" alt="Igor Fonseca" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Igor Fonseca</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 alunos já passaram por uma de duas explosões.
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 40,00</strong>
+              <strong>R$ {teacher.cost}</strong>
         </p>
         <button type="button"><img src={whatsappIcon} alt="WhatsApp" />
             Entrar em contato</button>
