86311c68a06bd2869163f5090678205532357b74
Igor Fonseca <igorfonsecaigor@gmail.com>
2020-08-13 00:42:13 -0300

    Criando TeacherItem no projeto mobile

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -32,7 +32,7 @@ function StudyTabs() {
           marginLeft: 16,
         },
         inactiveBackgroundColor: '#fafafc',
-        activeBackgroundColor: '#ebevf5',
+        activeBackgroundColor: '#ebebf5',
         inactiveTintColor: '#c1bccc',
         activeTintColor: '#32264d'
       }}
@@ -42,9 +42,9 @@ function StudyTabs() {
         component={TeacherList} 
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size }) => {
+          tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-easel" size={size} color={color} />
+              <Ionicons name="ios-easel" size={size} color={focused ? '#8257e5' : color} />
             )
           }
         }}
@@ -54,9 +54,9 @@ function StudyTabs() {
         component={Favorites} 
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size }) => {
+          tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-heart" size={size} color={color} />
+              <Ionicons name="ios-heart" size={size} color={focused ? '#8257e5' : color} />
             )
           }
         }}
